7d70c41af267abf55feb6b734b686c5b00ae119d
AustinPaley <austinpaley32@gmail.com>
2021-12-07 17:54:18 -0500

    Initila Menu Renders

--- src/App.js
+++ src/App.js
@@ -4,13 +4,223 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <form>
-        <label>
-          Yumyums
-          <input type="radio" name="Yumyums" />
-        </label>
-      </form>
-    </div>
+<section class="themed themed-  show-separator-False page-layout-whole subpage  singleplatformmenupage odd">
+<section class="sp-menu-wrapper">
+<div class="container">
+<div class="page-header inside text-center">
+<div class="title-wrapper">
+<h2 class="title">Dinner Menu</h2>
+</div>
+</div>
+<div class="sections text-left">
+<div class="section dontsplit" data-slug="starters-and-salads">
+<div class="title-wrapper">
+<h2 class="title">Starters and Salads</h2>
+<div class="descr">Add Protein to Your Salad: Chicken or Shrimp $9</div>
+</div>
+<div class="items">
+<div class="item">
+<div class="name text-uppercase">Soup du Jour</div>
+<div class="descr">Chef's daily choice</div>
+<div class="price">9</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">French Onion Soup</div>
+<div class="descr">Caramelized onion, aged gruyere, crouton</div>
+<div class="price">12</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Salmon Cake</div>
+<div class="descr">Blistered tomato, caper dill aioli</div>
+<div class="price">15</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Escargot en Croute</div>
+<div class="descr">Pernod, provencal butter, puff pastry</div>
+<div class="price">16</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">House Made Chicken Liver Pate</div>
+<div class="descr">Cornichons, pickled onion, whole grain mustard and baguette</div>
+<div class="price">16</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Crispy Goat Cheese</div>
+<div class="descr">Beet puree, blackberries, basil pesto</div>
+<div class="price">16</div>
+</div>
+<div class="item"><div class="name text-uppercase">Organic Hummus</div>
+<div class="descr">Choice of pita or vegetables</div>
+<div class="price">13</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Zucchini Noodles</div>
+<div class="descr">Basil pesto, shaved almonds and parmesan</div>
+<div class="price">13</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Caesar</div>
+<div class="descr">Romaine, toasted croutons, shaved parmesan</div>
+<div class="price">15</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Arugula Salad</div>
+<div class="descr">Apples, orange segments, goat cheese, roasted almonds, citrus vinaigrette</div>
+<div class="price">17</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Mixed Baby Field Greens</div>
+<div class="descr">Pears, grapes, walnuts, shaved parmesan, tarragon vinaigrette</div>
+<div class="price">15</div>
+</div>
+</div>
+</div>
+<div class="section dontsplit" data-slug="entrees">
+<div class="title-wrapper">
+<h2 class="title">Entrees</h2>
+</div>
+<div class="items">
+<div class="item">
+<div class="name text-uppercase">Roasted Free Range Chicken*</div>
+<div class="descr">Dauphinoise potato, tri color carrots, rosemary jus</div>
+<div class="price">27</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Coq au Vin*</div>
+<div class="descr">Red wine braised chicken, mushrooms, carrots, pearl onion over mashed potato</div>
+<div class="price">27</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Duck Confit*</div>
+<div class="descr">Duck leg, fingerling potato, haricot verts, caramelized onions, brandied cherry reduction</div>
+<div class="price">24</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Capellini Pomodoro</div>
+<div class="descr">Goat cheese and pine nuts</div>
+<div class="price">18</div>
+<div class="choice clearfix name-1 unit-0">
+<div class="name pull-left">Add Chicken or Shrimp for</div>
+<div class="price pull-left">9</div>
+</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Grilled Scottish Salmon*</div>
+<div class="descr">French lentils, thyme jus</div>
+<div class="price">27</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Pan Roasted Yellowfin Tuna*</div>
+<div class="descr">Arugula salad, avocado mouse, sauteed broccoli, cilantro lime soy</div>
+<div class="price">29</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Branzino*</div>
+<div class="descr">Cauliflower puree, sauteed broccoli, lobster reduction</div>
+<div class="price">28</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Beef Bourguignon*</div>
+<div class="descr">Red wine, slow braised beef, pearl onions, cherry tomato, mushrooms, served atop mashed potato</div>
+<div class="price">28</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Steak Frites*</div>
+<div class="descr">Grass fed 12oz new york strip steak, served with hand-cut fries</div>
+<div class="price">36</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Steak au Poivre*</div>
+<div class="descr">Grass fed 12oz new york strip steak, peppercorn brandy sauce, served with dauphinoise potato</div>
+<div class="price">38</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">FR Burger*</div>
+<div class="descr">Pat la frieda, short rib and brisket blend, 8oz burger with bibb lettuce, tomato, red onion, house sauce served with hand-cut fries or salad</div>
+<div class="price">19</div>
+<div class="choice clearfix name-1 unit-0">
+<div class="name pull-left">Add Applewood Smoked Bacon</div>
+<div class="price pull-left">3</div>
+</div>
+<div class="choice clearfix name-1 unit-0">
+<div class="name pull-left">Add Cheese, Avocado, Sauteed Onions</div>
+<div class="price pull-left">2</div>
+</div>
+</div>
+</div>
+</div>
+<div class="section dontsplit" data-slug="moules-frites">
+<div class="title-wrapper">
+<h2 class="title">Moules Frites</h2>
+<div class="descr">$23. Served with hand-cut fries. Choice of broth</div>
+</div>
+<div class="items">
+<div class="item">
+<div class="name text-uppercase">Mariniere</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Lobster</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Catalan</div>
+<div class="descr">Contains chorizo</div>
+</div>
+</div>
+</div>
+<div class="section dontsplit" data-slug="plats-daccompagnement">
+<div class="title-wrapper">
+<h2 class="title">Plats d'Accompagnement</h2>
+<div class="descr">$7 each or 3 for $18</div>
+</div>
+<div class="items">
+<div class="item">
+<div class="name text-uppercase">Sauteed Spinach</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Sauteed Broccoli</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Haricot Verts</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Filed Greens</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Tri-Color Carrots</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Sweet Potato Fries</div>
+</div>
+<div class="item">
+<div class="name text-uppercase">Hand-Cut Fries</div>
+</div>
+</div>
+</div>
+<div class="section dontsplit" data-slug="assiette-de-fromage">
+<div class="title-wrapper">
+<h2 class="title">Assiette de Fromage</h2>
+</div>
+<div class="items">
+<div class="item">
+<div class="name text-uppercase">Assiette De Fromage</div>
+<div class="descr">Chef's daily selection of cheese, dried fruit, nuts and baguette</div>
+<div class="choice clearfix name-1 unit-0">
+<div class="name pull-left">Three Cheeses</div>
+<div class="price pull-left">18</div>
+</div>
+<div class="choice clearfix name-1 unit-0">
+<div class="name pull-left">Four Cheeses</div>
+<div class="price pull-left">20</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="menu-footnote text-center">Vegetarian: may contain dairy or eggs. *Consuming raw or undercooked meats, poultry, seafood, shellfish or eggs may increase your risk of food borne ill</div>
+</div>
+</section>
+</section>
+</div>
   );
 }
 
